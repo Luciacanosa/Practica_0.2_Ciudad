7fdbc4652328ff16f6ba40e3150fc4fae737ff79
lulutrarala <4350lucia@gmail.com>
2025-11-24 13:45:04 +0100

    layout resumen kyoto pag principal

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // efectos para el cursor
 
-$(document).mousemove(function(e) {
-  $(".cursor").css({
-    left: e.pageX + "px",
-    top: e.pageY + "px"
-  });
-});
+// $(document).mousemove(function(e) {
+//   $(".cursor").css({
+//     left: e.pageX + "px",
+//     top: e.pageY + "px"
+//   });
+// });
 
-$("a").hover(
-  function() {
-    $(".cursor").css({ transform: "translate(-50%, -50%) scale(1.5)" });
-  },
-  function() {
-    $(".cursor").css({ transform: "translate(-50%, -50%) scale(1)" });
-  }
-);
+// $("a").hover(
+//   function() {
+//     $(".cursor").css({ transform: "translate(-50%, -50%) scale(1.5)" });
+//   },
+//   function() {
+//     $(".cursor").css({ transform: "translate(-50%, -50%) scale(1)" });
+//   }
+// );
