275a55d30750fba8355138a6c3675c3e1c1de4ac
lulutrarala <4350lucia@gmail.com>
2025-11-25 18:33:15 +0100

    carrusel imagenes pag principal

--- js/script.js
+++ js/script.js
@@ -15,3 +15,23 @@
 //     $(".cursor").css({ transform: "translate(-50%, -50%) scale(1)" });
 //   }
 // );
+
+
+// carrusel fotos parte historia pag principal
+function iniciarCarrusel(selector) {
+  const images = document.querySelectorAll(selector + " img");
+  let index = 0;
+
+  function showNextImage() {
+    images.forEach(img => img.classList.remove("active"));
+    images[index].classList.add("active");
+    index = (index + 1) % images.length;
+  }
+
+  showNextImage(); // mostrar la primera
+  setInterval(showNextImage, 3000); // cambiar cada 3 segundos
+}
+
+// Inicia cada carrusel por separado
+iniciarCarrusel(".carrusel.historia");
+iniciarCarrusel(".carrusel.cultura");
