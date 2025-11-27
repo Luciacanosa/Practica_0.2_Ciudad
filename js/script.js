d2e1ead984dfca0ed490942e5d3bac65d3c83c36
lulutrarala <4350lucia@gmail.com>
2025-11-27 11:07:35 +0100

    ventana modal y cursor raton

--- js/script.js
+++ js/script.js
@@ -1,20 +1,58 @@
 // efectos para el cursor
 
-// $(document).mousemove(function(e) {
-//   $(".cursor").css({
-//     left: e.pageX + "px",
-//     top: e.pageY + "px"
-//   });
-// });
+$(document).mousemove(function (e) {
+  $(".cursor").css({
+    left: e.clientX + "px",  // 👈 usa clientX/clientY
+    top: e.clientY + "px"
+  });
+});
 
-// $("a").hover(
-//   function() {
-//     $(".cursor").css({ transform: "translate(-50%, -50%) scale(1.5)" });
-//   },
-//   function() {
-//     $(".cursor").css({ transform: "translate(-50%, -50%) scale(1)" });
-//   }
-// );
+// opcional: efecto al pasar por enlaces
+$("a").hover(
+  function () {
+    $(".cursor").addClass("cursor-grow");
+  },
+  function () {
+    $(".cursor").removeClass("cursor-grow");
+  }
+);
+
+// ventana modal ULTIMA OPORTUNIDAD JRPASS EN 10 SEGUNDOs
+
+// Lógica abrir/cerrar de la ventana modal
+let btnOpenModal = document.querySelector("#openModal");
+
+btnOpenModal.addEventListener("click", openModalWindow);
+
+// Función propia para abrir ventana modal
+function openModalWindow() {
+  let modalWindow = document.querySelector("#modalWindow");
+  modalWindow.classList.add("show-modal");
+}
+
+let btnCloseModal = document.querySelector(
+  "#modalWindow > .modal-content > .close"
+);
+
+btnCloseModal.addEventListener("click", closeModalWindow);
+
+// Función para cerrar la ventana modal
+function closeModalWindow() {
+  let modalWindow = document.querySelector("#modalWindow");
+  modalWindow.classList.remove("show-modal");
+}
+
+// Cerrar ventana modal cuando se detecta click fuera
+window.addEventListener("click", function (event) {
+  let modal = document.querySelector("#modalWindow");
+  if (event.target == modal) {
+    closeModalWindow();
+  }
+});
+
+// 👇 Abrir modal automáticamente a los 10 segundos
+setTimeout(openModalWindow, 10000);
+
 
 
 // carrusel fotos parte historia pag principal
