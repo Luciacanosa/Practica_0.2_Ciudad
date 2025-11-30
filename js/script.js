6f4740b84955cceb57122f77e1ef5c06f9e2b29d
lulutrarala <4350lucia@gmail.com>
2025-11-30 18:10:46 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -24,6 +24,23 @@ $("a").hover(
   }
 );
 
+
+// menu hambuerguesa para el navbar
+// lógica del menu (simplemente el clik en un icono)
+
+// selección de elementos del html
+
+let burger = document.querySelector(".burger > i");
+let menu_opt = document.querySelector(".menu");
+
+// definimos evneto click sobre le botón del menú
+burger.addEventListener("click", function () {
+  burger.classList.toggle("fa-bars");
+  burger.classList.toggle("fa-times");
+  menu_opt.classList.toggle("menu-show");
+});
+
+
 // ventana modal ULTIMA OPORTUNIDAD JRPASS EN 10 SEGUNDOs
 
 // Lógica abrir/cerrar de la ventana modal
